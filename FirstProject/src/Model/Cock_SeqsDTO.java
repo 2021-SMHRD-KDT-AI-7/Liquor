5e1e6c624fa31896a0d03a1f295b5ca5980d70dc
smhrd <smhrd@121.147.52.246>
2021-11-24 17:25:35 +0900

    시퀀스만가져오는 DAO DTO

---
+++ FirstProject/src/Model/Cock_SeqsDTO.java
@@ -0,0 +1,21 @@
+package Model;
+
+public class Cock_SeqsDTO {
+	private int cocktail_seq;
+
+	
+	
+	public Cock_SeqsDTO(int cocktail_seq) {
+		super();
+		this.cocktail_seq = cocktail_seq;
+	}
+
+	public int getCocktail_seq() {
+		return cocktail_seq;
+	}
+
+	public void setCocktail_seq(int cocktail_seq) {
+		this.cocktail_seq = cocktail_seq;
+	}
+	
+}
