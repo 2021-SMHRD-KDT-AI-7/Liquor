8fb20752bf8e57b84bf5f91414f11bf1a3ab53d6
smhrd <smhrd@210.223.239.217>
2021-11-27 12:26:40 +0900

    컨트롤러 추가

---
+++ FirstProject/src/Controller/LoadMyRecipeServiceCon.java
@@ -0,0 +1,35 @@
+package Controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import Model.DAO;
+import Model.MemberDTO;
+
+@WebServlet("/LoadMyRecipeServiceCon")
+public class LoadMyRecipeServiceCon extends HttpServlet {
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setCharacterEncoding("EUC-KR");
+		HttpSession session = request.getSession();
+		MemberDTO info = (MemberDTO)session.getAttribute("info");
+		String id = info.getId();
+		
+		DAO dao = new DAO();
+		
+		ArrayList<String[]> my_recipe = new ArrayList<>();
+		my_recipe = dao.loadMyRecipe(id);
+		
+		session.setAttribute("my_recipe", my_recipe);
+		
+		response.sendRedirect("myRecipe.jsp");	
+	}
+
+}
